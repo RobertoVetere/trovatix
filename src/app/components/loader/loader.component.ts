16fe59f0e0b223026ae714c47db9bc893bba5c60
Roberto Vetere <Roberto.vetere@gmail.com>
2024-08-29 11:07:39 +0200

    add animation

--- src/app/components/loader/loader.component.ts
+++ src/app/components/loader/loader.component.ts
@@ -14,13 +14,20 @@ export class LoaderComponent implements OnInit{
   constructor(private elementRef: ElementRef) {}
 
   ngOnInit(): void {
-    lottie.loadAnimation({
-      container: this.elementRef.nativeElement.querySelector('#lottie-animation'), // Elemento contenedor
-      renderer: 'svg',
-      loop: true,
-      autoplay: true,
-      path: 'assets/loader-animation.json' // Ruta al archivo JSON de Lottie
-    });
-  }
+  setTimeout(() => {
+    const container = this.elementRef.nativeElement.querySelector('#lottie-animation');
+    if (container) {
+      lottie.loadAnimation({
+        container: container, // Asegura que el contenedor esté presente
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: 'assets/loader-animation.json' // Ruta al archivo JSON de Lottie
+      });
+    } else {
+      console.error('Contenedor no encontrado para la animación Lottie');
+    }
+  }, 0); 
+}
 
 }
